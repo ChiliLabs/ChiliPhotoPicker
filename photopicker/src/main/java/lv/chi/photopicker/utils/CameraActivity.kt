434c791e95b199a47e29fa15fd729f36241d78d7
abdulrahmanGamal <abdogamal480@gmail.com>
2020-09-26 07:40:12 +0200

    use camerax instead opening normal cam

--- photopicker/src/main/java/lv/chi/photopicker/utils/CameraActivity.kt
+++ photopicker/src/main/java/lv/chi/photopicker/utils/CameraActivity.kt
@@ -4,54 +4,63 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.hardware.display.DisplayManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
+import android.util.DisplayMetrics
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import lv.chi.photopicker.utils.CameraActivity.Request.IMAGE_CAPTURE
+import kotlinx.android.synthetic.main.activity_camera.*
+import lv.chi.photopicker.R
 import java.io.File
-
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 internal class CameraActivity : AppCompatActivity() {
-
-    private lateinit var output: Uri
-
     private var permissionGranted: Boolean = false
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private var displayId: Int = -1
+    private lateinit var processCameraProvider: ProcessCameraProvider
+
+    private val displayManager by lazy {
+        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+
+    private val displayListener = object : DisplayManager.DisplayListener {
+        override fun onDisplayAdded(displayId: Int) = Unit
+        override fun onDisplayRemoved(displayId: Int) = Unit
+        override fun onDisplayChanged(displayId: Int) {
+            if (displayId == this@CameraActivity.displayId) {
+                imageCapture?.targetRotation = preview.display.rotation
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= 23) {
             permissionGranted = hasCameraPermission()
         } else permissionGranted = true
 
-        if (savedInstanceState == null) {
-            output = provideImageUri()
-            if (permissionGranted) requestImageCapture()
-            else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.CAMERA),
-                    Request.CAMERA_ACCESS_PERMISSION
-                )
-            }
-        } else savedInstanceState.getParcelable<Uri>(Key.OUTPUT)?.let {
-            output = it
-        }
-    }
+        overridePendingTransition(R.anim.slide_up, R.anim.hold)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+        capture.setOnClickListener { takePhoto() }
+        close.setOnClickListener { finish() }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == Request.IMAGE_CAPTURE) {
-            if (resultCode == RESULT_OK) {
-                setResult(RESULT_OK, Intent().setData(output))
-            } else {
-                contentResolver.delete(output, null, null)
-            }
-            finish()
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        displayManager.registerDisplayListener(displayListener, null)
+
+        preview.post {
+            displayId = preview.display.displayId
+            tryStartPreview()
         }
     }
 
@@ -63,44 +72,134 @@ internal class CameraActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == Request.CAMERA_ACCESS_PERMISSION && hasCameraPermission()) {
             permissionGranted = true
-            requestImageCapture()
+            startPreview()
         } else onBackPressed()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable(Key.OUTPUT, output)
-    }
 
-    private fun provideImageUri() = createTempFile(
-        suffix = ".jpg",
-        directory = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Pictures").apply { mkdirs() }
-    )
-        .apply { deleteOnExit() }
-        .providerUri(this)
-
-    private fun requestImageCapture() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (cameraIntent.resolveActivity(packageManager) != null) {
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, output)
-            startActivityForResult(cameraIntent, IMAGE_CAPTURE)
-        }
-    }
     private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
         this,
         Manifest.permission.CAMERA
     ) == PackageManager.PERMISSION_GRANTED
 
     private object Request {
-        const val IMAGE_CAPTURE = 1
         const val CAMERA_ACCESS_PERMISSION = 2
     }
 
-    private object Key {
-        const val OUTPUT = "output"
+
+    override fun onResume() {
+        super.onResume()
+        if (imageCapture == null && permissionsGranted()) {
+            startPreview()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+        displayManager.unregisterDisplayListener(displayListener)
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.hold, R.anim.slide_down)
+    }
+
+    private fun tryStartPreview() {
+        startPreview()
+    }
+
+    private fun startPreview() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            try {
+                val metrics = DisplayMetrics().also { preview.display.getRealMetrics(it) }
+                val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+                val rotation = preview.display.rotation
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                val previewUseCase = Preview.Builder()
+                    .setTargetAspectRatio(screenAspectRatio)
+                    .setTargetRotation(rotation)
+                    .build()
+
+                imageCapture = ImageCapture.Builder()
+                    .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                    .setTargetAspectRatio(screenAspectRatio)
+                    .setTargetRotation(rotation)
+                    .build()
+
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, previewUseCase, imageCapture
+                )
+                previewUseCase.setSurfaceProvider(preview.createSurfaceProvider())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: run {
+            tryStartPreview()
+            return
+        }
+
+        val photoFile = File(outputDirectory, FILENAME)
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(photoFile)
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(e: ImageCaptureException) {
+                    e.printStackTrace()
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    setResult(RESULT_OK, Intent().setData(output.savedUri))
+                    finish()
+                }
+            })
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, "Pictures").apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    private fun permissionsGranted(): Boolean {
+        return PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
     }
 
     companion object {
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+        private const val FILENAME = "camera.jpg"
         fun createIntent(context: Context) = Intent(context, CameraActivity::class.java)
+
     }
+
 }
